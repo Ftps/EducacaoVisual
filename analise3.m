e205fab8b074a5a31dcd2474eff210be7cc713c3
PedroMMFernandes <52373420+PedroMMFernandes@users.noreply.github.com>
2019-06-30 15:37:17 +0100

    Update analise3.m

--- analise3.m
+++ analise3.m
@@ -19,14 +19,28 @@ a4 = m(:,A4);
 
 % representar graficamente a variação temporal das grandezas medidas
 figure();
+
 subplot(2,2,1);
 plot(time, a1);
+xlabel('t (s)');
+ylabel('a1 (m/s^2) ');
+
 subplot(2,2,2);
 plot(time, a2);
+xlabel('t (s)');
+ylabel('a2 (m/s^2) ');
+
 subplot(2,2,3);
 plot(time, a3);
+xlabel('t (s)');
+ylabel('a3 (m/s^2) ');
+
 subplot(2,2,4);
 plot(time, a4);
+xlabel('t (s)');
+ylabel('a4 (m/s^2) ');
+
+suptitle('representacao temporal das aceleracoes');
 
 
 T = 0.004;
@@ -41,29 +55,39 @@ subplot(2,2,1);
 dsms1 = fft(a1,NFFT)/L;
 ssms1 = 2*abs(dsms1(1:(NFFT/2+1)));
 plot(ff, ssms1);
-
+xlabel('f (Hz)');
+ylabel('|a1| (m/s^2)');
 %picos em 1,007/5,266 2,991/3,04 10,01/2,8 19,99/0,9311
 
 subplot(2,2,2);
 dsms2 = fft(a2,NFFT)/L;
 ssms2 = 2*abs(dsms2(1:(NFFT/2+1)));
 plot(ff, ssms2);
-
+xlabel('f (Hz)');
+ylabel('|a2| (m/s^2)');
 %pico em 3,998/3,725
 
 subplot(2,2,3);
 dsms3 = fft(a3,NFFT)/L;
 ssms3 = 2*abs(dsms3(1:(NFFT/2+1)));
 plot(ff, ssms3);
-
+xlabel('f (Hz)');
+ylabel('|a3| (m/s^2)');
 %pico em 2,014/5,437 3,998/3,717
 
 subplot(2,2,4);
 dsms4 = fft(a4,NFFT)/L;
 ssms4 = 2*abs(dsms4(1:(NFFT/2+1)));
 plot(ff, ssms4);
-
+xlabel('f (Hz)');
+ylabel('|a4| (m/s^2)');
 %pico em 2,014/5,445
+
+suptitle('espetro unilateral de amplitude de cada aceleracao');
+
+%utilizar a funcao data cursor para determinar os maximos 
+
+
 
 % função para ler os valores dos ficheiros
 function mat = data_read(fp)
@@ -98,8 +122,4 @@ end
 function n = size_m(v)
     [n, ~] = size(v);
 end
-
-
-
-
 
