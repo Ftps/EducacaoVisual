b0da7875d807f5f0d63fa1ee336acad301daba70
Ftps <francisco.tomas@tecnico.ulisboa.pt>
2019-07-07 11:28:28 +0100

    Anal2 done

--- analise2.m
+++ analise2.m
@@ -6,7 +6,7 @@ fclose(fp);
 
 N = [2, 1.7; 3, 2.7; 4, 3.7; 5.5, 5.2; 7, 6.7; 0, 0.3; -1, -0.7; -2.5, -2.2];
 N1 = 1; N2 = 2;
-N_count = zeros(size_lin(N));
+N_count = zeros(1, size_lin(N));
 
 Time=1; EAS=2; QNE=3; a_z=4;
 g=9.80665;
@@ -28,29 +28,34 @@ suptitle('Representacao temporal da velocidade de rotacao, consumo e temperatura
 
 
 G_acel=m(:,a_z)/g;
-G_picos=G_acel(1);
+G_picos=[m(1, Time), G_acel(1)];
 sinal=0;
 for i=2:size_lin(G_acel)
     ddt=(G_acel(i)-G_acel(i-1))/(m(i,Time)-m(i-1,Time));
 
     if (sinal ~= sign(ddt) && sign(ddt)~= 0)
-    G_picos=[G_picos; G_acel(i)];
+    G_picos=[G_picos; m(i, Time), G_acel(i)];
     sinal=sign(ddt);
-    if sinal
-    G_picos=[G_picos, G_acel(i)];
     end
 end
 
 out_file = "picos_acel.txt";
 fp = fopen(out_file,'w');
-
+fprintf(fp,'%6s %12s\r\n','time','acel');
+for i = 1:size_lin(G_picos)
+    fprintf(fp,'%6.2f %12.8f\r\n',G_picos(i,1), G_picos(i,2));
+end
 fclose(fp);
 
-sinal = 0;
 for i=1:size_lin(N)
-    for j=1:size_col(G_acel)
-
-
+    sinal = 0;
+    for j=1:size_lin(G_acel)
+        if ~sinal && G_acel(j) >= N(i, N1)
+            N_count(i) = N_count(i) + 1;
+            sinal = 1;
+        elseif sinal && G_acel(j) < N(i, N2)
+            sinal = 0;
+        end
     end
 end
 
