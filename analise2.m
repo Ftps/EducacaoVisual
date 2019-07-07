302a0b363bf045e7a21137d77d772a3c7e452f4a
Ftps <francisco.tomas@tecnico.ulisboa.pt>
2019-07-07 10:29:22 +0100

    Update

--- analise2.m
+++ analise2.m
@@ -4,6 +4,10 @@ fp = fopen(file,'r');
 m = data_read(fp);
 fclose(fp);
 
+N = [2, 1.7; 3, 2.7; 4, 3.7; 5.5, 5.2; 7, 6.7; 0, 0.3; -1, -0.7; -2.5, -2.2];
+N1 = 1; N2 = 2;
+N_count = zeros(size_lin(N));
+
 Time=1; EAS=2; QNE=3; a_z=4;
 g=9.80665;
 
@@ -29,13 +33,23 @@ for i=2:size_lin(G_acel)
     ddt=(G_acel(i)-G_acel(i-1))/(m(i,Time)-m(i-1,Time));
     sinal=sign(ddt);
     if sinal
-    G_picos=[G_picos, G_acel(i)]
+    G_picos=[G_picos, G_acel(i)];
+    end
 end
 
 out_file = "picos_acel.txt";
 fp = fopen(out_file,'w');
 
 fclose(fp);
+
+sinal = 0;
+for i=1:size_lin(N)
+    for j=1:size_col(G_acel)
+        
+
+    end
+end
+
 
 % funcao para ler os valores dos ficheiros
 function mat = data_read(fp)
